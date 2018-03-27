82691595928a8092673f02f76becf20fd6cf88b5
ArtAmmirati <34355879+ArtAmmirati@users.noreply.github.com>
2018-03-27 19:08:21 -0400

    Firstupload

--- ChapterFiveCode/Program.cs
+++ ChapterFiveCode/Program.cs
@@ -16,7 +16,7 @@ namespace ChapterFiveCode
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AccountBalance());
         }
     }
 }
