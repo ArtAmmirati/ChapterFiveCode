8dbc4a12c361cd21c5fabf33a3b2549a4e14f561
ArtAmmirati <34355879+ArtAmmirati@users.noreply.github.com>
2018-03-27 20:37:47 -0400

    Turorial5-3

---
+++ Tutorial5-3forLoop/SpeedConverter.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Tutorial5_3forLoop
+{
+    partial class speedConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.displayBTN = new System.Windows.Forms.Button();
+            this.exitBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(13, 13);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(252, 147);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // displayBTN
+            // 
+            this.displayBTN.Location = new System.Drawing.Point(22, 193);
+            this.displayBTN.Name = "displayBTN";
+            this.displayBTN.Size = new System.Drawing.Size(98, 48);
+            this.displayBTN.TabIndex = 1;
+            this.displayBTN.Text = "Display Speeds";
+            this.displayBTN.UseVisualStyleBackColor = true;
+            this.displayBTN.Click += new System.EventHandler(this.displayBTN_Click);
+            // 
+            // exitBTN
+            // 
+            this.exitBTN.Location = new System.Drawing.Point(162, 193);
+            this.exitBTN.Name = "exitBTN";
+            this.exitBTN.Size = new System.Drawing.Size(98, 48);
+            this.exitBTN.TabIndex = 2;
+            this.exitBTN.Text = "Exit";
+            this.exitBTN.UseVisualStyleBackColor = true;
+            this.exitBTN.Click += new System.EventHandler(this.exitBTN_Click);
+            // 
+            // speedConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.exitBTN);
+            this.Controls.Add(this.displayBTN);
+            this.Controls.Add(this.outputListBox);
+            this.Name = "speedConverter";
+            this.Text = "Speed Converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button displayBTN;
+        private System.Windows.Forms.Button exitBTN;
+    }
+}
+
---
+++ Tutorial5-3forLoop/SpeedConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial5_3forLoop
+{
+    public partial class speedConverter : Form
+    {
+        public speedConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void displayBTN_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //constants
+                const int START_SPEED = 60;
+                const int END_SPEED = 130;
+                const int INTERVAL = 10;
+                const double CONVERSION_FACTOR = 0.6214;
+
+                //variables
+                int kph;        //Kilometers per hour
+                double mph;     //Miles per hour
+
+                // display the table of speeds
+                for (kph = START_SPEED; kph <= END_SPEED; kph += INTERVAL)
+                {
+                    //calculate miles per hour
+                    mph = kph * CONVERSION_FACTOR;
+
+                    //display the  conversion
+                    outputListBox.Items.Add(kph + "  KPH is the same as " +
+                        mph.ToString ("n2") + " MPH");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+
+        private void exitBTN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
